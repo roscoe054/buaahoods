d07600d11264b44fcdb4911d85477457f0e2cb8b
Roscoe <heiwu805@hotmail.com>
2015-03-04 23:22:01 +0800

    test route

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | path to your installation.
 |
  */
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
