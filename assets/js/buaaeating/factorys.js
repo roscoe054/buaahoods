130d6576006bfd5156aaf5e65d2de0f429f490ff
Roscoe <heiwu805@hotmail.com>
2015-03-07 23:47:52 +0800

    route update

--- assets/js/buaaeating/factorys.js
+++ assets/js/buaaeating/factorys.js
@@ -36,12 +36,12 @@ buaaeatingFactorys.factory('Data', function() {
 
 	return {
 		dishes: [
-			DishType.createNew("101", "红烧猪排盖饭", "15", "猪排+青菜+鸡蛋饼", "/assets/images/hongshaopaigu.jpg"),
-			DishType.createNew("102", "香菇鸡丁盖饭", "15", "鸡块+香菇+鸡蛋饼", "/assets/images/xianggujiding.jpg"),
-			DishType.createNew("103", "酱香排骨盖饭", "15", "排骨+土豆+鸡蛋饼", "/assets/images/jiangxiangpaigu.jpg"),
-			DishType.createNew("104", "蒜苔炒肉盖饭", "13", "猪肉+蒜苔+鸡蛋饼", "/assets/images/suantaichaorou.jpg"),
-			DishType.createNew("105", "香干炒肉盖饭", "13", "猪肉+香干+鸡蛋饼", "/assets/images/xiangganchaorou.jpg"),
-			DishType.createNew("106", "土豆丝炒肉盖饭", "13", "猪肉+土豆丝+鸡蛋饼", "/assets/images/tudousi.jpg")
+			DishType.createNew("101", "红烧猪排盖饭", "15", "猪排+青菜+鸡蛋饼", "/waimai/assets/images/hongshaopaigu.jpg"),
+			DishType.createNew("102", "香菇鸡丁盖饭", "15", "鸡块+香菇+鸡蛋饼", "/waimai/assets/images/xianggujiding.jpg"),
+			DishType.createNew("103", "酱香排骨盖饭", "15", "排骨+土豆+鸡蛋饼", "/waimai/assets/images/jiangxiangpaigu.jpg"),
+			DishType.createNew("104", "蒜苔炒肉盖饭", "13", "猪肉+蒜苔+鸡蛋饼", "/waimai/assets/images/suantaichaorou.jpg"),
+			DishType.createNew("105", "香干炒肉盖饭", "13", "猪肉+香干+鸡蛋饼", "/waimai/assets/images/xiangganchaorou.jpg"),
+			DishType.createNew("106", "土豆丝炒肉盖饭", "13", "猪肉+土豆丝+鸡蛋饼", "/waimai/assets/images/tudousi.jpg")
 		],
 		drinks: [
 			DrinkType.createNew("可乐"),
