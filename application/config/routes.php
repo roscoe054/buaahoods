a5bd68e6402b9ff184ac44accc5a68261b8c7d4a
Roscoe <heiwu805@hotmail.com>
2015-04-12 16:29:49 +0800

    reset root

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,5 @@
-<?php if (!defined('BASEPATH')) {
-	exit('No direct script access allowed');
-}
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -26,7 +25,7 @@
 | RESERVED ROUTES
 | -------------------------------------------------------------------------
 |
-| There area two reserved routes:
+| There are three reserved routes:
 |
 |	$route['default_controller'] = 'welcome';
 |
@@ -36,35 +35,20 @@
 |
 |	$route['404_override'] = 'errors/page_missing';
 |
-| This route will tell the Router what URI segments to use if those provided
-| in the URL cannot be matched to a valid route.
+| This route will tell the Router which controller/method to use if those
+| provided in the URL cannot be matched to a valid route.
 |
- */
-$routeRoot = '';
-
-// 获取优惠码
-$route['discount/get_discount/(:any)'] = 'discount/get_discount/$1';
-$route['discount/get_discount'] = 'discount/get_discount';
-
-// 查询订单
-$route['order/query_order/(:any)'] = 'order/query_order/$1';
-$route['order/query_order'] = 'order/query_order';
-
-// 提交订单
-$route['order/submit_order/(:any)'] = 'order/submit_order/$1';
-$route['order/submit_order'] = 'order/submit_order';
-
-$route['buaaeating/reserve'] = 'buaaeating/reserve';
-$route['buaaeating/(:any)'] = 'buaaeating/view/$1';
-
-$route['news/json_get_news'] = 'news/json_get_news';
-$route['news/create'] = 'news/create';
-$route['news/(:any)'] = 'news/view/$1';
-$route['news'] = 'news';
-
-$route['(:any)'] = 'buaaeating/reserve';
-$route['404_override'] = '';
+|	$route['translate_uri_dashes'] = FALSE;
+|
+| This is not exactly a route, but allows you to automatically route
+| controller and method names that contain dashes. '-' isn't a valid
+| class or method name character, so it requires translation.
+| When you set this option to TRUE, it will replace ALL dashes in the
+| controller and method URI segments.
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+*/
 $route['default_controller'] = 'welcome';
-
-/* End of file routes.php */
-/* Location: ./application/config/routes.php */
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
