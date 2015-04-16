8a9b67b335f092d8995b51a78193ba19da95a063
Roscoe <heiwu805@hotmail.com>
2015-04-16 23:18:12 +0800

    test

--- assets/js/buaaeating/app.js
+++ assets/js/buaaeating/app.js
@@ -1,5 +1,5 @@
 var buaaeatingApp = angular.module('buaaeatingApp',[
-	'ngRoute','ngStorage','buaaeatingCtrls','buaaeatingFilters','buaaeatingFactorys'
+	'ngRoute','ngTouch','ngStorage','buaaeatingCtrls','buaaeatingFilters','buaaeatingFactorys'
 ]);
 
 var rootPath = '/partials/buaaeating/'
